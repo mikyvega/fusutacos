c37848d28c170f3d322b4faba591230f112d8844
mikyvega <133449548+mikyvega@users.noreply.github.com>
2023-05-12 20:28:58 -0600

    quitandoelpost

--- guardar.php
+++ guardar.php
@@ -17,11 +17,11 @@ if (mysqli_connect_errno($conn)) {
 }
 
 //Create an Insert prepared statement and run it
-$escrito = $_POST['comentario'];
+
 echo "Vamos al query";
 if ($stmt = mysqli_prepare($conn, "INSERT INTO comentarios (Comentario) VALUES (?)")) {
     echo "Aqui esta en el prepare";
-    mysqli_stmt_bind_param($stmt, 'ssd', $escrito);
+    mysqli_stmt_bind_param($stmt, 'ssd', "Probando la informacion");
     mysqli_stmt_execute($stmt);
     printf("Insert: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
     mysqli_stmt_close($stmt);
