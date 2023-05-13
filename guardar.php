b562dd076705dd83dcfdaef4a37c6a49940e7e37
mikyvega <133449548+mikyvega@users.noreply.github.com>
2023-05-12 19:00:24 -0600

    logs

--- guardar.php
+++ guardar.php
@@ -4,10 +4,15 @@ $username = 'myadmin';
 $password = 'Maistro3003$';
 $db_name = 'ProyectoFinal';
 
+echo "Declaraciones de variables";
+
 //Establishes the connection
 $conn = mysqli_init();
+echo "Inicializacion";
 mysqli_ssl_set($conn,NULL,NULL, "/home/site/wwwroot/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+echo "SSL";
 mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL);
+echo "Connecion Correcta";
 if (mysqli_connect_errno($conn)) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
