b82c02a79c894b6285b0772c51f0e8d3110bbe95
mikyvega <133449548+mikyvega@users.noreply.github.com>
2023-05-12 22:41:08 -0600

    otra version

--- guardar.php
+++ guardar.php
@@ -1,32 +1,22 @@
 <?php
+
 $host = 'fusutaqueria.mysql.database.azure.com';
 $username = 'myadmin';
 $password = 'Maistro3003$';
 $db_name = 'fusutaqueria';
 
-echo "Declaraciones de variables ";
-
-//Establishes the connection
+//Initializes MySQLi
 $conn = mysqli_init();
-echo "Inicializacion...";
-mysqli_real_connect($conn, $host, $username, $password, $db_name);
-echo "Connecion Correcta";
-if (mysqli_connect_errno($conn)) {
-    echo "Failed to connect to MySQL";
-    die('Failed to connect to MySQL: '.mysqli_connect_error());    
-}
 
-//Create an Insert prepared statement and run it
-echo htmlspecialchars($_POST['comentario']);
-echo "Vamos al query";
-if ($stmt = mysqli_prepare($conn, "INSERT INTO comentarios (Comentario) VALUES (?)")) {
-    echo "Aqui esta en el prepare";
-    mysqli_stmt_bind_param($stmt, 'ssd', "Probando la informacion");
-    mysqli_stmt_execute($stmt);
-    printf("Insert: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
-    mysqli_stmt_close($stmt);
+mysqli_ssl_set($conn,NULL,NULL, "/home/site/wwwroot/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+
+// Establish the connection
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, NULL, MYSQLI_CLIENT_SSL);
+
+//If connection failed, show the error
+if (mysqli_connect_errno())
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 echo "Escribido";
-// Close the connection
-mysqli_close($conn);
 ?>
