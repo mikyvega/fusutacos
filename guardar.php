7c92bb5e3966560343a2568d34f265ff2ae89cf7
mikyvega <133449548+mikyvega@users.noreply.github.com>
2023-05-12 18:53:34 -0600

    subiendo SSL

--- guardar.php
+++ guardar.php
@@ -6,7 +6,7 @@ $db_name = 'ProyectoFinal';
 
 //Establishes the connection
 $conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_ssl_set($conn,NULL,NULL, "/home/site/wwwroot/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL);
 if (mysqli_connect_errno($conn)) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
