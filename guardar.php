ed64a06f777ea0e95363b4adfa476df83d6d8092
mikyvega <133449548+mikyvega@users.noreply.github.com>
2023-05-12 18:42:38 -0600

    probando otra forma de conectarnos

--- guardar.php
+++ guardar.php
@@ -1,24 +1,25 @@
 <?php
-$servername = "fusutaqueria.mysql.database.azure.com";
-$database = "fusutaqueria";
-$username = "myadmin";
-$password = "Maistro3003$";
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
-// Check connection
-if (!$conn) {
-      die("Connection failed: " . mysqli_connect_error());
-}
- 
-echo "Conectado ";
+$host = 'fusutaqueria.mysql.database.azure.com';
+$username = 'myadmin';
+$password = 'Maistro3003$';
+$db_name = 'ProyectoFinal';
 
-$escrito=trim($_POST['comentario']);
-$sql = "INSERT INTO Comentarios VALUES ('$escrito')";
-//INSERT INTO solo`(Texto`) VALUES ('[value-1]')
-if (mysqli_query($conn, $sql)) {
-      echo "Texto Guardado";
-} else {
-      echo "Error: no se guarda" . $sql . "<br>" . mysqli_error($conn);
+//Establishes the connection
+$conn = mysqli_init();
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+if (mysqli_connect_errno($conn)) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
+
+//Create an Insert prepared statement and run it
+$escrito=trim($_POST['comentario']);
+if ($stmt = mysqli_prepare($conn, "INSERT INTO Comentarios (comentario) VALUES (?)")) {
+mysqli_stmt_bind_param($stmt, 'ssd', $escrito);
+mysqli_stmt_execute($stmt);
+printf("Insert: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
+mysqli_stmt_close($stmt);
+}
+
+// Close the connection
 mysqli_close($conn);
 ?>
